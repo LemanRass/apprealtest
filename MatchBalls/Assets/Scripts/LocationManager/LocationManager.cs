56b88f478c2d4ad03ccac2dc1d08b4927cb32e75
LemanRass <btoomonline@gmail.com>
2021-03-24 17:19:38 +0200

    Balls explosions

--- MatchBalls/Assets/Scripts/LocationManager/LocationManager.cs
+++ MatchBalls/Assets/Scripts/LocationManager/LocationManager.cs
@@ -55,7 +55,57 @@ public class LocationManager : MonoBehaviour
                     ballView.Init(ballType);
                     ballViews.Add(ballView);
                     ballView.rigidbody.AddRelativeForce(ballsSpawnPoint.transform.up * 20.0f, ForceMode2D.Impulse);
+                    ballView.onMouseDown += OnBallClicked;
                 }
+            }
+        }
+    }
+
+    private async Task DestroyBall(BallView ballView)
+    {
+        await ballView.Explode();
+        ballView.onMouseDown -= OnBallClicked;
+        ballViews.Remove(ballView);
+        ballView.Dispose();
+    }
+
+    private void OnBallClicked(BallView ballView)
+    {
+        var linkedBalls = new List<BallView>();
+        GetLinkedBalls(ballView, ref linkedBalls);
+        if (linkedBalls.Count >= 3)
+        {
+            linkedBalls.ForEach(n => DestroyBall(n));
+        }
+        else
+        {
+            ballView.Decoy();
+        }
+    }
+
+    private void GetLinkedBalls(BallView ballView, ref List<BallView> linkedBalls)
+    {
+        var allNearbyBalls = ballView.GetCollideWith();
+        var matchBalls = allNearbyBalls.FindAll(n => n.ballType == ballView.ballType);
+
+        if(matchBalls.Count > 0)
+        {
+            //Remove already listed balls
+            for(int i = 0; i < linkedBalls.Count; i++)
+            {
+                if(matchBalls.Contains(linkedBalls[i]))
+                {
+                    matchBalls.Remove(linkedBalls[i]);
+                }
+            }
+
+            //Add new matched balls
+            linkedBalls.AddRange(matchBalls);
+
+            //Run recursive search of others balls
+            for(int i = 0; i < matchBalls.Count; i++)
+            {
+                GetLinkedBalls(matchBalls[i], ref linkedBalls);
             }
         }
     }
@@ -64,8 +114,7 @@ public class LocationManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
-            var ballType = CalculateNextBall();
-            SpawnBall(ballType);
+
         }
     }
 }
--- MatchBalls/Assets/Scripts/Views/BallView.cs
+++ MatchBalls/Assets/Scripts/Views/BallView.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public class BallView : MonoBehaviour
@@ -6,15 +9,57 @@ public class BallView : MonoBehaviour
     public CircleCollider2D collider;
     public Rigidbody2D rigidbody;
 
-    private BallType ballType;
+    public BallType ballType { get; private set; }
+
+    public event Action<BallView> onMouseDown = null; 
 
     public void Init(BallType ballType)
     {
         this.ballType = ballType;
     }
 
-    public List<BallView> CollideWith()
+    public List<BallView> GetCollideWith()
     {
-        return null;
+        var balls = new List<BallView>();
+        var colliders = Physics2D.OverlapCircleAll(transform.position, collider.radius + (collider.radius / 10.0f));
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            var ball = colliders[i].GetComponent<BallView>();
+            if (ball == null)
+                continue;
+
+            balls.Add(ball);
+        }
+
+        return balls;
+    }
+
+    public async Task Decoy()
+    {
+        var tween = transform.DOScale(1.5f, 0.1f);
+        await tween.AsyncWaitForCompletion();
+
+        tween = transform.DOScale(0.8f, 0.05f);
+        await tween.AsyncWaitForCompletion();
+
+        tween.Kill();
+    }
+
+    public async Task Explode()
+    {
+        var tween = transform.DOScale(2.0f, 0.05f);
+        await tween.AsyncWaitForCompletion();
+        tween.Kill();
+    }
+
+    public void Dispose()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnMouseDown()
+    {
+        onMouseDown?.Invoke(this);
     }
 }
